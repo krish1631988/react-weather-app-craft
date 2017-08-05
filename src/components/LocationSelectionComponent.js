3f62e3bea8a1afc905a26e7f4b3394cb2b5cefc3
skrishnamachari1 <shrikanth_krishnamachari@intuit.com>
2017-08-05 02:34:41 -0700

    HUI-4839: Added util for getting cities based on state. Added states for LocationSelectionComponent to start with

--- src/components/LocationSelectionComponent.js
+++ src/components/LocationSelectionComponent.js
@@ -1,10 +1,87 @@
 import React, { Component } from 'react';
+import { getUniqueCitiesForThisState } from '../util/fetch_states_cities';
 
 class LocationSelectionComponent extends Component {
+
+  /**
+   * Constructor to start with initializations for state and binding
+   * handlers for listeners. Handling state for State and City selection.
+   * @param props props object to be used within component.
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityState: "ca",
+      citiesInState: [],
+      selectedCity: ""
+    };
+
+    this.handleCityStateChange = this.handleCityStateChange.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
+  }
+
+  /**
+   * Let us set the state partially based on state value.
+   */
+  componentWillMount() {
+    const cityEntries = getUniqueCitiesForThisState(this.state.cityState);
+    this.setState({
+      citiesInState: cityEntries,
+      selectedCity: cityEntries[0]
+    });
+  }
+
+  /**
+   * Handler method for onChange event on State select element.
+   * @param event onChnage event object emitted by select.
+   */
+  handleCityStateChange(event) {
+    const cityStateValue = event.target.value;
+    const cityEntries = getUniqueCitiesForThisState(cityStateValue);
+    this.setState({
+      cityState: event.target.value,
+      citiesInState: cityEntries,
+      selectedCity: cityEntries[0]
+    });
+  }
+
+  /**
+   * Handler method for onChange event on City select element.
+   * @param event onChnage event object emitted by select.
+   */
+  handleCityChange(event) {
+    this.setState({selectedCity: event.target.value});
+  }
+
+  /**
+   * Sub-render method to render City select element.
+   * @param event onChnage event object emitted by select.
+   */
+  renderCitySelect() {
+    const selectedCity = this.state.selectedCity;
+    const citiesInState = this.state.citiesInState;
+    const createCityOptions = (city) => {
+      return <option key={city.toLowerCase()} value={city.toLowerCase()}>{city}</option>;
+    };
+    return (<select
+            value={selectedCity}
+            onChange={this.handleCityChange}
+          >
+            {citiesInState.map(createCityOptions)}
+          </select>);
+  }
+
   render() {
     return (
       <div>
-        <p>LocationSelectionComponent to hold the widget to select location.</p>
+        <select value={this.state.cityState} onChange={this.handleCityStateChange}>
+          <option value="az">AZ</option>
+          <option value="tx">TX</option>
+          <option value="ca">CA</option>
+          <option value="ny">NY</option>
+        </select>
+        {this.renderCitySelect()}
+        <p>At present the city is {this.state.selectedCity}</p>
       </div>
     );
   }
---
+++ src/util/fetch_states_cities.js
@@ -0,0 +1,24 @@
+const cities = require("cities");
+
+/**
+ * Function to get all the entries which mention cities in object.
+ * @param stateAbbr Abbreviation for state.
+ */
+function getCityEntriesForState(stateAbbr) {
+  const allEntries = cities.findByState(stateAbbr);
+  const cityEntries = [];
+  for(var i=0; i<allEntries.length; i++) {
+    cityEntries.push(allEntries[i].city);
+  }
+  return cityEntries;
+}
+
+/**
+ * Function to get unique city entries for the given state. Here we can pass
+ * cityEntries as an iterable object to ES6 Set native object. It would create
+ * a unique set which we can convert back to array by using spread operator.
+ * @param stateAbbr Abbreviation for state.
+ */
+export function getUniqueCitiesForThisState(stateAbbr) {
+  return [...new Set(getCityEntriesForState(stateAbbr))]
+}
